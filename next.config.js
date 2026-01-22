26792a861053671d751057c8d7ce833bf53ca0c7
SquidSecure <SquidSecure@gmail.com>
2026-01-22 11:32:50 +0100

    Fix: Configuration images Next.js et chemins absolus

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
+    unoptimized: true, // IMPORTANT pour Vercel avec images statiques
+  },
+  // Optionnel : Si vous avez des vidéos
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/videos/',
+          outputPath: 'static/videos/',
+          name: '[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
   },
 };
 
